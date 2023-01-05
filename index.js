84beb804e9ec1f8acc0331d3c682e50ad547d5cb
GoldenDev1002 <jenzielol1002@gmail.com>
2023-01-05 14:28:05 +0000

    user route

--- index.js
+++ index.js
@@ -42,3 +42,14 @@ app.get("/lessons", (req, res)=> {
    // res.send("to the new file ");
 })
 
+app.get("/user", (req, res)=> {
+    app.set("json spaces", 3);
+    let userDetails = [
+        {
+            email: "firstuser1@gmail.com",
+            password : "password134"
+              
+           },
+    ]
+    res.json(userDetails)
+})
